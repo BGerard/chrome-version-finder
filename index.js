18747064c18fd361cbc66166221215f2e68719c2
ben <bengerard@hotmail.co.uk>
2020-07-27 21:43:47 +0100

    Pass version number in via command line args

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const { getAllVersionTags, getBasePosition } = require("./versionFinder");
 
+const isValidVersion = (version) => (/\d{2}/.test(version));
+
 const findPackage = async (versionToFind, basePosition) => {
     const fields = 'items(kind,mediaLink,metadata,name,size,updated),kind,prefixes,nextPageToken';
     const res = await fetch(`https://www.googleapis.com/storage/v1/b/chromium-browser-snapshots/o?delimiter=/&prefix=Linux_x64/${basePosition}/&fields=${fields}`);
@@ -14,16 +16,22 @@ const findPackage = async (versionToFind, basePosition) => {
     return findPackage(basePosition-1);
 };
 
-const main = async (versionToFind) => {
-    const versionsByMajor = await getAllVersionTags();
-    const versionTag = versionsByMajor[versionToFind];
-    const basePosition = await getBasePosition(versionTag);
-    const mediaLink = await findPackage(versionToFind, basePosition);
-    return {
-        version: versionTag,
-        basePosition: basePosition,
-        url: mediaLink
-    };
+const main = async () => {
+    const versionToFind = parseInt(process.argv[2], 0);
+    if (isValidVersion(versionToFind)) {
+        const versionsByMajor = await getAllVersionTags();
+        const versionTag = versionsByMajor[versionToFind];
+        const basePosition = await getBasePosition(versionTag);
+        const mediaLink = await findPackage(versionToFind, basePosition);
+        return {
+            version: versionTag,
+            basePosition: basePosition,
+            url: mediaLink
+        };
+    } else {
+        console.error('Invalid major version number');
+        process.exit(0);
+    }
 };
 
-main(50).then(console.log, console.err);
+main().then(console.log, console.error);
