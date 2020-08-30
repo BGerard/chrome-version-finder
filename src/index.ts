7ef71f6f6d2df68084de48e3580412096e2f49c3
ben <bengerard@hotmail.co.uk>
2020-08-30 19:41:36 +0100

    Print package url by default

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
+import { isString } from "lodash";
 import {VersionTags} from "./versionFinder";
 import { getAllVersionTags, getBasePosition } from "./versionFinder";
 import {findPackage} from "./packageFinder";
 
 const isValidVersion = (version: string) => (/\d{2}/.test(version));
+const enableVerbosePrint = (flag: string) => isString(flag) && flag === '--verbose';
 
 const main = async () => {
     const versionToFind: string = process.argv[2];
+    const verbosePrint: boolean = enableVerbosePrint(process.argv[3]);
     if (isValidVersion(versionToFind)) {
         const versionsByMajor: VersionTags = await getAllVersionTags();
         const versionTag = versionsByMajor[versionToFind];
         const basePosition: number = await getBasePosition(versionTag);
         const mediaLink: string = await findPackage(basePosition);
+        if (!verbosePrint) {
+            return mediaLink;
+        }
         return {
             version: versionTag,
             basePosition: basePosition,
