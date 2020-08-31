9a3152a2067589a839d340856ae1927b20e5ccda
ben <bengerard@hotmail.co.uk>
2020-08-31 13:13:07 +0100

    Add prettier & format

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
 import { isString } from "lodash";
-import {VersionTags} from "./versionFinder";
+import { VersionTags } from "./versionFinder";
 import { getAllVersionTags, getBasePosition } from "./versionFinder";
-import {findPackage} from "./packageFinder";
+import { findPackage } from "./packageFinder";
 
-const isValidVersion = (version: string) => (/\d{2}/.test(version));
-const enableVerbosePrint = (flag: string) => isString(flag) && flag === '--verbose';
+const isValidVersion = (version: string) => /\d{2}/.test(version);
+const enableVerbosePrint = (flag: string) =>
+  isString(flag) && flag === "--verbose";
 
 const main = async () => {
-    const versionToFind: string = process.argv[2];
-    const verbosePrint: boolean = enableVerbosePrint(process.argv[3]);
-    if (isValidVersion(versionToFind)) {
-        const versionsByMajor: VersionTags = await getAllVersionTags();
-        const versionTag = versionsByMajor[versionToFind];
-        const basePosition: number = await getBasePosition(versionTag);
-        const mediaLink: string = await findPackage(basePosition);
-        if (!verbosePrint) {
-            return mediaLink;
-        }
-        return {
-            version: versionTag,
-            basePosition: basePosition,
-            url: mediaLink
-        };
-    } else {
-        console.error('Invalid major version number');
-        process.exit(0);
+  const versionToFind: string = process.argv[2];
+  const verbosePrint: boolean = enableVerbosePrint(process.argv[3]);
+  if (isValidVersion(versionToFind)) {
+    const versionsByMajor: VersionTags = await getAllVersionTags();
+    const versionTag = versionsByMajor[versionToFind];
+    const basePosition: number = await getBasePosition(versionTag);
+    const mediaLink: string = await findPackage(basePosition);
+    if (!verbosePrint) {
+      return mediaLink;
     }
+    return {
+      version: versionTag,
+      basePosition: basePosition,
+      url: mediaLink,
+    };
+  } else {
+    console.error("Invalid major version number");
+    process.exit(0);
+  }
 };
 
 main().then(console.log, console.error);
--- src/versionFinder.ts
+++ src/versionFinder.ts
@@ -3,37 +3,48 @@ import * as cheerio from "cheerio";
 import { chain } from "lodash";
 import * as compareVersions from "compare-versions";
 
-const isChromiumVersion = (version: string): boolean => /\d+\.\d+\.\d+\.\d+/.test(version);
+const isChromiumVersion = (version: string): boolean =>
+  /\d+\.\d+\.\d+\.\d+/.test(version);
 
 export interface VersionTags {
-    [key: string]: string
+  [key: string]: string;
 }
 
 export const getAllVersionTags = async (): Promise<VersionTags> => {
-    const res = await fetch('https://chromium.googlesource.com/chromium/src.git/+refs');
-    const html = await res.text();
-    const $ = cheerio.load(html);
-    const set = $('.RefList').get(1);
-    const setName = $(set).find('h3').text();
-    if (setName === 'Tags') {
-        const versionNumberElements: string[] = $(set).find('ul > li > a').map(function() {
-            // @ts-ignore
-            return $(this).text();
-        }).get();
-        return chain(versionNumberElements)
-            .filter(isChromiumVersion)
-            .sort(compareVersions)
-            .groupBy((versionString: string) => versionString.split('.')[0])
-            .map((versionParts: string[], versionString: string) => [versionString, versionParts[0]])
-            .fromPairs()
-            .value();
-    } else {
-        throw new Error('Cannot find Chromium versions.');
-    }
+  const res = await fetch(
+    "https://chromium.googlesource.com/chromium/src.git/+refs"
+  );
+  const html = await res.text();
+  const $ = cheerio.load(html);
+  const set = $(".RefList").get(1);
+  const setName = $(set).find("h3").text();
+  if (setName === "Tags") {
+    const versionNumberElements: string[] = $(set)
+      .find("ul > li > a")
+      .map(function () {
+        // @ts-ignore
+        return $(this).text();
+      })
+      .get();
+    return chain(versionNumberElements)
+      .filter(isChromiumVersion)
+      .sort(compareVersions)
+      .groupBy((versionString: string) => versionString.split(".")[0])
+      .map((versionParts: string[], versionString: string) => [
+        versionString,
+        versionParts[0],
+      ])
+      .fromPairs()
+      .value();
+  } else {
+    throw new Error("Cannot find Chromium versions.");
+  }
 };
 
 export const getBasePosition = async (versionTag: string): Promise<number> => {
-  const res = await fetch(`https://omahaproxy.appspot.com/deps.json?version=${versionTag}`);
+  const res = await fetch(
+    `https://omahaproxy.appspot.com/deps.json?version=${versionTag}`
+  );
   const { chromium_base_position } = await res.json();
   return chromium_base_position;
 };
